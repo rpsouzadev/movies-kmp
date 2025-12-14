a7a6232099c333341c647aee3b4248ae4a0facd4
Rafael Souza <rpsouza.dev@gmail.com>
2025-12-14 19:26:54 -0300

    feat: Implements the cast member item component

---
+++ composeApp/src/commonMain/kotlin/com/rpsouza/movies/presentation/components/castmemberitem/CastMemberItem.kt
@@ -0,0 +1,86 @@
+package com.rpsouza.movies.presentation.components.castmemberitem
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import coil3.size.Dimension
+import com.rpsouza.movies.presentation.theme.Dimens
+import com.rpsouza.movies.presentation.theme.MoviesAppTheme
+import movies.composeapp.generated.resources.Res
+import movies.composeapp.generated.resources.minecraft
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CastMemberItem(
+    modifier: Modifier = Modifier,
+    profilePictureUrl: String,
+    name: String,
+    character: String,
+) {
+    Surface(
+        modifier = modifier
+            .height(Dimens.Dp76),
+        shape = MaterialTheme.shapes.medium,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            // Usar Image e depois vamos mudar para AsyncImage
+            Image(
+                painter = painterResource(Res.drawable.minecraft),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .clip(
+                        MaterialTheme.shapes.medium.copy(
+                            topEnd = ZeroCornerSize,
+                            bottomEnd = ZeroCornerSize
+                        )
+                    ),
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = Dimens.Dp16)
+            ) {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                Text(
+                    text = character,
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CastMemberItemPreview() {
+    MoviesAppTheme {
+        CastMemberItem(
+            profilePictureUrl = "",
+            name = "Will Smith",
+            character = "Christopher Gardner",
+        )
+    }
+}
